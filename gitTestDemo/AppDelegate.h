1a5ff9a41958e4dbe9bb34ca34036c12a3215bd7
xcsun <xc@xcdemacbook-pro.local>
2016-08-16 11:05:43 +0800

    Initial Commit

---
+++ gitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTestDemo
+//
+//  Created by xc on 16/8/16.
+//  Copyright © 2016年 xc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTestDemo
+//
+//  Created by xc on 16/8/16.
+//  Copyright © 2016年 xc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
